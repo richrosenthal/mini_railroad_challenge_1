212e20cbbe118daee904dec30fef445c3be7cabf
richrosenthal <rrosenthal2013@gmail.com>
2026-09-05 10:27:12 -0400

    adds sorting functions to shipments by priority of shipment

--- challenge_3.py
+++ challenge_3.py
@@ -24,3 +24,32 @@ print(f"Shipment priorities loaded: {len(shipment_priorities)}")
 print(f"Rail connections loaded: {len(rail_connections)}")
 print()
 print("READY FOR ROUTE PLANNING")
+
+
+for shipment_priority in shipment_priorities:
+    print(
+        f"{shipment_priority['shipment_type']} has priority "
+        f"{shipment_priority['priority']}."
+    )
+
+for shipment in shipments:
+    print(shipment)
+
+#Priority Lookup
+priority_lookup = {}
+for priority_row in shipment_priorities:
+    shipment_type = priority_row["shipment_type"]
+    #int()conversion - cSV data always in as string so convert "1" to 1 to allow numeric sorting
+    priority_number = int(priority_row["priority"])
+    priority_lookup[shipment_type] = priority_number
+
+#Add Priority to each shipment using lookup
+for shipment in shipments:
+    shipment_type = shipment["shipment_type"]
+    shipment["priority"] = priority_lookup[shipment_type]
+
+#Sorting shipments by priority
+shipments.sort(key=lambda shipment: shipment["priority"])
+
+for shipment in shipments:
+    print(shipment)
